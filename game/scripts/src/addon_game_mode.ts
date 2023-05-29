d1d1d73678424ca6ff734ce2d51ab6776f9f5375
Marcelina Boyer <132597697+bgrutlmut50950@users.noreply.github.com>
2023-05-30 02:52:18 +0800

    Update addon_game_mode.ts

--- game/scripts/src/addon_game_mode.ts
+++ game/scripts/src/addon_game_mode.ts
@@ -1,10 +1,17 @@
 import 'utils/index';
 import { ActivateModules } from './modules';
 import Precache from './utils/precache';
+import { GameMode } from './GameMode';
 
 Object.assign(getfenv(), {
     Activate: () => {
         ActivateModules();
+        GameMode.Activate();
     },
     Precache: Precache,
 });
+
+//重载地图
+if (GameRules.Addon) {
+    GameRules.Addon.Reload();
+}
